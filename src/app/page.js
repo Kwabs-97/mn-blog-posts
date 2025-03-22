8d50c3156809779b8b91eb08424462881b6f11d6
Samuel <khobewayne@gmail.com>
2025-03-22 11:46:32 +0000

    loading states and error handling on home screen complete

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,26 @@ import Image from "next/image";
 import { usePost, useDeletePost } from "@/hooks/usePosts";
 import PostCard from "@/components/PostCard";
 import { useState } from "react";
+import LoadingSpinner from "@/components/Loading-Spinner";
 
 export default function Home() {
+  const [page, setPage] = useState("");
+  const [category, setCategory] = useState("");
+
+  const {isLoading, isError, error, data} = usePost()
+  if (isLoading) {
+    return <LoadingSpinner />
+  }
+
+  if(isError){
+    <div className="h-screen w-screen px-5 flex items-center justify-center">
+<p className="text-red-400 font-semibold">{`Error loading page. -${error}. Please try again later`}</p>
+
+    </div>
+  }
   return (
   <div>
+
   </div>
   );
 }
