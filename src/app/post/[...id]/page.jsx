79fd21b61d56df3d4f1b4068b3a18d2e97ff096f
Samuel <khobewayne@gmail.com>
2025-03-22 18:30:28 +0000

    extract dynamic parameter

--- src/app/post/[...id]/page.jsx
+++ src/app/post/[...id]/page.jsx
@@ -1,6 +1,12 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { useDeletePost, usePost } from "@/hooks/usePosts";
+import { useParams, useRouter } from "next/navigation";
 
 function page() {
+  const {} = useParams();
+  const {} = usePost;
   return <div>page</div>;
 }
 
