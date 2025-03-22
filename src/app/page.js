efb5ab21039b35c1b7b848563c7ff555ccc697c1
Samuel <khobewayne@gmail.com>
2025-03-22 11:37:57 +0000

    post card component complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+'use client'
 import Image from "next/image";
+import { usePost, useDeletePost } from "@/hooks/usePosts";
+import PostCard from "@/components/PostCard";
+import { useState } from "react";
 
 export default function Home() {
   return (
-  <>
-  </>
+  <div>
+  </div>
   );
 }
