c73c1915240c67ced43998a41d1079fba0f73b67
Samuel <khobewayne@gmail.com>
2025-03-22 09:55:58 +0000

    update api and useUpdatePost updated

--- src/app/hooks/usePosts.js
+++ src/app/hooks/usePosts.js
@@ -5,17 +5,14 @@ import { addPost, updatePost as updatePostAction, deletePost as deletePostAction
 
 
 
-export function usePosts(){
+export function usePosts(page, limit, search, category){
     return useQuery({
         queryKey:['posts', page, limit, search, category],
         queryFn: () => api.getPosts(page, limit, search, category)
     });
-
-    
 }
 
 export function usePost(id){
-
     return useQuery({
         queryKey: ['post',id],
         queryFn: () => api.getPost(id),
@@ -24,13 +21,13 @@ export function usePost(id){
 }
 
 export function useCreatePost(){
-    const useQueryClient = useQueryClient();
+    const queryClient = useQueryClient();
     const dispatch = useAppDispatch();
     return useMutation({
         mutationFn:(postData) => api.createPost(postData),
         onSuccess: (newPost) => {
             dispatch(addPost(newPost))
-            QueryClient.invalidateQueries({queryKey:['posts']})
+            queryClient.invalidateQueries({queryKey:['posts']})
         }
     })
 }
@@ -39,15 +36,13 @@ export function useUpdatePost(){
     const queryClient = useQueryClient();
     const dispatch = useAppDispatch();
     return useMutation({
-        mutationFn: (id, postData) => api.updatePost(id,postData),
+        mutationFn: (postData) => api.updatePost(postData.id, postData.data),
         onSuccess: (updatedPost) => {
-            dispatch(updatePostAction(updatedPost)),
-            queryClient.invalidateQueries({queryKey:['posts']}),
-            queryClient.invalidateQueries({queryKey:['post', updatedPost.id]})
+            dispatch(updatePostAction(updatedPost));
+            queryClient.invalidateQueries({queryKey:['posts']});
+            queryClient.invalidateQueries({queryKey:['post', updatedPost.id]});
         } 
     })
-     
-    
 }
 
 export function useDeletePost(){
