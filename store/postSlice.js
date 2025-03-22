8cbaa96fc1b0ee026102192318455f67f6507557
Samuel <khobewayne@gmail.com>
2025-03-22 06:25:58 +0000

    postSlice, store provider complete

---
+++ store/postSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const intialState = {
+    posts:[],
+    currentPost: null,
+    loading: false,
+    error: null,
+    filters: {
+        search: "",
+        category: "",
+        limit: 10,
+        page:1
+    },
+    totalPosts: 0
+}
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        setPosts: (state, action) => {
+            state.posts = action.payload
+        },
+        setCurrenPost: (state, action) => {
+            state.currentPost= action.payload
+        },
+        setLoading:(state, action)=>{
+            state.loading = action.payload
+        },
+        setError:(state, action)=>{
+            state.error = action.payload
+        },
+        setFilters: (state, action) => {
+            state.filters = {...state.filters, ...action.payload} 
+        },
+        setTotalPosts: (state, action) => {
+            state.totalPosts = action.payload
+        },
+        addPost: (state, action) => {
+            state.posts.unshift(action.payload)
+        },
+        updatePost:(state, action) =>{
+           const index  = state.posts.findIndex(post => post.id === action.payload.id);
+            if ( index !== -1 ) {
+                state.posts[index] = action.payload
+            }
+        },
+        deletePost: (state, action) => {
+            state.posts = state.posts.filter(post => post.id !== action.payload)
+        }
+    }
+})
+
+export const {
+    setCurrenPost,
+    setError,
+    setFilters,
+    setLoading,
+    setPosts,setTotalPosts,
+    addPost,
+    deletePost,
+    updatePost
+} = postSlice.actions
+
+export default postSlice.reducer;
