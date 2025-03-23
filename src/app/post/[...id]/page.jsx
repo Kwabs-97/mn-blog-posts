b593a8cdda7322ac9587ad401b9b20dca78a6439
Samuel <khobewayne@gmail.com>
2025-03-23 15:23:11 +0000

    update posts state management

--- src/app/post/[...id]/page.jsx
+++ src/app/post/[...id]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useDeletePost, usePost } from "@/hooks/usePosts";
 import { useParams, useRouter } from "next/navigation";
